cf45dbdbcf7c133e741f86bbfd4f90c1e27ae7df
charan <aluricharan1922@gmail.com>
2024-02-24 12:41:31 +0530

    update code for getting product info

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -18,7 +18,7 @@ const createProduct = async (req, res) => {
 const getProduct = async (req, res) => {
   try {
     const { id } = req.params;
-    const products = await Product.find();
+    const products = await Product.findById(id);
     res.status(200).json(products);
   } catch (error) {
     res.status(500).json({ error: error.message });
